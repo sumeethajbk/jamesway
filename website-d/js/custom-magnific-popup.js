2c45bbc2f9acc1516a4e58ca232410b81566c8c6
sumeetha <sumeetha@jbkinfotech.com>
2025-07-28 11:27:42 +0530

    first commit

---
+++ website-d/js/custom-magnific-popup.js
@@ -0,0 +1,60 @@
+jQuery(document).ready(function ($) {
+  function getYouTubeSrc() {
+    var isChrome =
+      /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
+    var baseSrc = "https://www.youtube.com/embed/%id%?autoplay=1&rel=0";
+    return isChrome ? baseSrc + "&mute=1" : baseSrc;
+  }
+
+  $(".popup-youtube").magnificPopup({
+    type: "iframe",
+    mainClass: "mfp-video",
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: true,
+    iframe: {
+      patterns: {
+        youtube: {
+          index: "youtube.com/",
+          id: "v=",
+          src: getYouTubeSrc(),
+        },
+      },
+    },
+  });
+
+  $(".popup-video").magnificPopup({
+    type: "iframe",
+    mainClass: "mfp-video",
+    removalDelay: 160,
+    preloader: false,
+    fixedContentPos: true,
+  });
+
+  $(".popup-modal").magnificPopup({
+    type: "inline",
+    fixedContentPos: true,
+    fixedBgPos: true,
+    overflowY: "auto",
+    preloader: false,
+    removalDelay: 160,
+    mainClass: "my-mfp-slide-top",
+  });
+
+  $(".work-row").magnificPopup({
+    delegate: "a",
+    gallery: {
+      enabled: true,
+    },
+    callbacks: {
+      elementParse: function (item) {
+        var href = item.el.attr("href");
+        if (href.match(/\.(jpg|jpeg|png|gif|webp)$/i)) {
+          item.type = "image";
+        } else {
+          item.type = "iframe";
+        }
+      },
+    },
+  });
+});
