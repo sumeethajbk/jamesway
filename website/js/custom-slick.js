3311c256c8063526dd2ea127bc25b657e09c055c
sumeetha <sumeetha@jbkinfotech.com>
2025-08-04 12:22:13 +0530

    first

--- website/js/custom-slick.js
+++ website/js/custom-slick.js
@@ -1,31 +1,32 @@
 jQuery(document).ready(function () {
 
-    /* Intro Slider */
+  /* Intro Slider */
   var windowWidth = jQuery(window).width();
+
   function introSlider() {
     var $introslide = jQuery('.intro-block-main');
     var introlist = $introslide.children().length;
     if (windowWidth <= 1023) {
-     
-        $introslide.slick({
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          speed: 2000,
-          dots: true,
-          arrows: false,
-          variableWidth: true,
-          draggable: true,
-          swipeToSlide: true,
-          touchThreshold: 100,
-          autoplay: false,
-          autoplaySpeed: 0,
-       
-          cssEase: 'linear',
-        });
-      
+
+      $introslide.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        speed: 2000,
+        dots: true,
+        arrows: false,
+        variableWidth: true,
+        draggable: true,
+        swipeToSlide: true,
+        touchThreshold: 100,
+        autoplay: false,
+        autoplaySpeed: 0,
+
+        cssEase: 'linear',
+      });
+
     } else {
-      
+
     }
   }
   introSlider();
@@ -37,64 +38,64 @@ jQuery(document).ready(function () {
       introSlider();
     }
   });
-/* End of Intro slider */
+  /* End of Intro slider */
 
-   /* Intro  row Slider */
+  /* Intro  row Slider */
   var windowWidth = jQuery(window).width();
-  function introSlider() {
-    var $introslide = jQuery('.intro-block-row');
-    var introlist = $introslide.children().length;
+
+  function introSliders() {
+    var $introslides = jQuery('.intro-block-row');
+    var introlists = $introslides.children().length;
     if (windowWidth <= 1023) {
-     
-        $introslide.slick({
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          infinite: true,
-          speed: 2000,
-          dots: true,
-          arrows: false,
-          variableWidth: true,
-          draggable: true,
-          swipeToSlide: true,
-          touchThreshold: 100,
-          autoplay: false,
-          autoplaySpeed: 0,
-       
-          cssEase: 'linear',
-        });
-      
+
+      $introslides.slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+        speed: 2000,
+        dots: true,
+        arrows: false,
+        variableWidth: true,
+        draggable: true,
+        swipeToSlide: true,
+        touchThreshold: 100,
+        autoplay: false,
+        autoplaySpeed: 0,
+
+        cssEase: 'linear',
+      });
+
     } else {
-      
+
     }
   }
-  introSlider();
+  introSliders();
 
   jQuery(window).on('resize load', function () {
     var newScreenWidth = jQuery(window).width();
     if (newScreenWidth !== windowWidth) {
       windowWidth = newScreenWidth;
-      introSlider();
+      introSliders();
     }
   });
-/* End of Intro row slider */
-    
-	
-/* Testimonial Slider */
-    jQuery(".testimonial-slider").slick({
-      slidesToShow: 1,
-      slidesToScroll: 1,
-      draggable: true,
-      swipeToSlide: true,
-      touchThreshold: 100,
-      infinite: false,
-      speed: 1000,
-      dots: false,
-      arrows: true,
-        adaptiveHeight:true,
-        prevArrow: '<div class="slick-arrow slick-prev flex flex-center" aria-label="Previous Arrow" role="button"><span><i class="fa-sharp fa-regular fa-arrow-left"></i></span></div>',
+  /* End of Intro row slider */
+
+
+  /* Testimonial Slider */
+  jQuery(".testimonial-slider").slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    draggable: true,
+    swipeToSlide: true,
+    touchThreshold: 100,
+    infinite: false,
+    speed: 1000,
+    dots: false,
+    arrows: true,
+    adaptiveHeight: true,
+    prevArrow: '<div class="slick-arrow slick-prev flex flex-center" aria-label="Previous Arrow" role="button"><span><i class="fa-sharp fa-regular fa-arrow-left"></i></span></div>',
     nextArrow: '<div class="slick-arrow slick-next flex flex-center" aria-label="Next Arrow" role="button"><span><i class="fa-sharp fa-regular fa-arrow-right"></i></span></div>',
-    });
-    
+  });
 
 
 });
